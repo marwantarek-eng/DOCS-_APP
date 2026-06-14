861ad7c8ccfaebdfb16ca7dd872f2fe6bf7d8c92
Marwan Tarek Ali Mahmoud Mohammed <marwantarek75@gmail.com>
2026-06-14 15:56:27 +0300

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,14 +2,14 @@ import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
       fontFamily: {
-        sans: ["Cairo", "DM Sans", "system-ui", "sans-serif"],
+        sans: ["Cairo", "system-ui", "sans-serif"],
       },
       colors: {
         navy: "#0B2545",
